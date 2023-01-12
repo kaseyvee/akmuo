bd83317864cd69d8c09e50eff6deefca1ddc93a9
kethnal <kaseyvaldez@hotmail.com>
2023-01-12 04:40:04 -0800

    Add icons to top menu

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -3,8 +3,10 @@ import './Feature.scss';
 export default function Feature() {
   return (
     <section className='Feature'>
-      <div className='header'>Spring is coming.</div>
-      <a href="" className='btn'>SHOP NEW ARRIVALS</a>
+      <div className='text'>
+        <div className='header'>Spring is coming.</div>
+        <a href="" className='btn'>SHOP NEW ARRIVALS</a>
+      </div>
     </section>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,15 @@ import './Navbar.scss';
 export default function Navbar() {
   return (
     <nav className="Navbar">
-      <div className='title'>AKMUO</div>
-      <div className='menu'>
+      <div className='title'>
+        AKMUO
+        <div className='user-menu'>
+          <a href=''><img src='https://i.imgur.com/KhgjqEF.png' alt='search icon' /></a>
+          <a href=''><img src='https://i.imgur.com/BHaQJi1.png' alt='current user' /></a>
+          <a href=''><img src='https://i.imgur.com/jjpvRmp.png' alt='cart' /></a>
+        </div>
+      </div>
+      <div className='shop-menu'>
         <a href=''>shop all</a>
         <a href=''>new arrivals</a>
         <a href=''>best sellers</a>
