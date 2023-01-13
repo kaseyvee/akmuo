b619648cbe7b6f06ebb00df9048653cb55a3494e
kethnal <kaseyvaldez@hotmail.com>
2023-01-13 03:28:30 -0800

    Add functionality to scroll buttons on feature carousel

--- src/components/FeatureItems.js
+++ src/components/FeatureItems.js
@@ -15,13 +15,23 @@ export default function FeatureItems() {
     )
   })
 
+  const screenWidth = document.documentElement.clientWidth;
+
+  function handleScrollLeft() {
+    document.getElementById('feature-scroll').scrollLeft -= screenWidth;
+  }
+
+  function handleScrollRight() {
+    document.getElementById('feature-scroll').scrollLeft += screenWidth;
+  }
+
   return (
     <section className="FeatureItems">
       <div className="feature-title">shop chunkies</div>
-      <div className='feature-scroll'>
-        <i class="fa-solid fa-angle-left"></i>
+      <div id='feature-scroll'>
+        <i class="fa-solid fa-angle-left" onClick={handleScrollLeft}></i>
         {items}
-        <i class="fa-solid fa-angle-right"></i>
+        <i class="fa-solid fa-angle-right" onClick={handleScrollRight}></i>
       </div>
     </section>
   );
